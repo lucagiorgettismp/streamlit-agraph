d4cf63c123122b54031d717446380f6b355d8b6b
Luca Giorgetti <lucagiorgit95@gmail.com>
2025-06-06 09:28:18 +0200

    Handle click and double click on both nodes and edges

--- streamlit_agraph/frontend/src/StreamlitVisGraph.tsx
+++ streamlit_agraph/frontend/src/StreamlitVisGraph.tsx
@@ -1,31 +1,92 @@
-import React from 'react';
-import VisGraph, {GraphData, GraphEvents, Options} from 'react-vis-graph-wrapper';
+import React, { useEffect, useRef } from 'react';
+import VisGraph, { GraphData, GraphEvents, Options } from 'react-vis-graph-wrapper';
 import { Streamlit } from "streamlit-component-lib";
 import { useRenderData } from "streamlit-component-lib-react-hooks";
 
 function StreamlitVisGraph() {
   const renderData = useRenderData();
 
-  const graphIn = JSON.parse(renderData.args["data"])
+  const graphIn = JSON.parse(renderData.args["data"]);
+  const options: Options = JSON.parse(renderData.args["config"]);
 
-  const options: Options = JSON.parse(renderData.args["config"])
+  const graph: GraphData = {
+    nodes: graphIn.nodes.slice(),
+    edges: graphIn.edges.slice()
+  };
 
-  const lookupNodeId = (lookupNode, myNodes) => myNodes.find(node => node.id === lookupNode);
+  const networkRef = useRef<any>(null); // riferimento alla rete
 
-  const graph: GraphData = {nodes: graphIn.nodes.slice(), edges: graphIn.edges.slice()}
+  // const prevGraphRef = useRef<{ nodes: any[], edges: any[] } | null>(null);
+
+  // useEffect(() => {
+  //   const prev = prevGraphRef.current;
+  //   const isDifferent = JSON.stringify(prev) !== JSON.stringify(graphIn);
+
+  //   if (networkRef.current && isDifferent) {
+  //     networkRef.current.fit({ animation: true });
+  //     prevGraphRef.current = graphIn;
+  //   }
+  // }, [graphIn]);
+
+  const getNodeById = (nodeId) => {
+    const element = graphIn.nodes.find(node => node.id === nodeId);
+    return element ? {
+      type: "node",
+      label: element.label,
+      color: element.color,
+      id: element.id
+    } : null;
+  };
 
   const events: GraphEvents = {
     selectNode: (event) => {
-      Streamlit.setComponentValue(event.nodes[0]);
-    }
-    ,
+      const node = getNodeById(event.nodes[0]);
+      // Streamlit.setComponentValue(node);
+      Streamlit.setComponentValue({
+        ...node,
+        eventType: "click"
+      });
+    },
+    selectEdge: (event) => {
+      const edge = graphIn.edges.find(edge => edge.id === event.edges[0]);
+      if (edge) {
+        Streamlit.setComponentValue({
+          type: "edge",
+          label: edge.label,
+          color: edge.color,
+          from: edge.from,
+          to: edge.to,
+          id: edge.id,
+          eventType: "click"
+        });
+      } else {
+        Streamlit.setComponentValue(null);
+      }
+    },
     doubleClick: (event) => {
-      const lookupNode = lookupNodeId(event.nodes[0], graph.nodes);
-      if (lookupNode && lookupNode.link) {
-        const link = lookupNode.link;
-        if (link) {
-          window.open(link);
+      if (event.nodes.length > 0) {
+          const node = getNodeById(event.nodes[0]);
+          if (node) {
+            Streamlit.setComponentValue({
+              ...node,
+              eventType: "doubleClick"
+            });
+          }
+      } else if (event.edges.length > 0) {
+        const edge = graphIn.edges.find(edge => edge.id === event.edges[0]);
+        if (edge) {
+          Streamlit.setComponentValue({
+            type: "edge",
+            label: edge.label,
+            color: edge.color,
+            from: edge.from,
+            to: edge.to,
+            id: edge.id,
+            eventType: "doubleClick"
+          });
         }
+      } else {
+        Streamlit.setComponentValue(null);
       }
     }
   };
@@ -36,8 +97,10 @@ function StreamlitVisGraph() {
         graph={graph}
         options={options}
         events={events}
-        ref = {(network: any) => {
-          // console.log(network)
+        ref={(networkInstance) => {
+          if (networkInstance) {
+            networkRef.current = networkInstance;
+          }
         }}
       />
     </span>
